ab691ce56b17264f491b08a2ca9ad9dce9686ffa
XRenso <70619857+XRenso@users.noreply.github.com>
2022-02-18 18:50:34 +1100

    Add files via upload

--- mega_info-prikaz_generala_gavsa/parser.py
+++ mega_info-prikaz_generala_gavsa/parser.py
@@ -10,31 +10,30 @@ news_url = 'https://sakhalin.info/'
 news_already = []
 rubriks_url=[]
 rubriks_name=[]
+
 def get_html(url):
 	response = requests.get(url, headers=headers)
 	if response.status_code == 200:
 		result = requests.get(url)
 	return result.text
 
-def get_title(html):
-	soup = BeautifulSoup(html, 'lxml')
-	title = soup.find('h5').text
-	return title
 
-def get_url(html):
-	soup = BeautifulSoup(html, 'lxml')
-	url = soup.find('h5').find('a').get('href')
-	return url
+
 def get_data(html):
 	soup = BeautifulSoup(html, 'lxml')
 	data = soup.find('span', class_='short-news_date').text
 	return data
-soup = BeautifulSoup(get_html(news_url), 'lxml')
-for a in soup.find_all('li', class_="dropdown-menu__item dropdown-menu__item_thin"):
-    i = a.find('a').get('href')
-    n = a.find('a').text
-    rubriks_url.append(i)
-    rubriks_name.append(n)
+    
+    
+def get_topics(html):
+    soup = BeautifulSoup(html, 'lxml')
+    for a in soup.find_all('li', class_="dropdown-menu__item dropdown-menu__item_thin"):
+        i = a.find('a').get('href')
+        n = a.find('a').text
+        rubriks_url.append(i)
+        rubriks_name.append(n)
 
-print(rubriks_url)
-print(rubriks_name)
+
+
+
+get_topics(get_html(news_url))
