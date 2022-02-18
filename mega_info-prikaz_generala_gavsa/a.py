be480ff6fd549afb178138f44ab513ec652491d5
ProgrammistDebilov <94727301+ProgrammistDebilov@users.noreply.github.com>
2022-02-18 22:31:13 +1100

    Add files via upload

--- mega_info-prikaz_generala_gavsa/a.py
+++ mega_info-prikaz_generala_gavsa/a.py
@@ -1,13 +1,26 @@
+from tkinter import *
 import parser
 import tkinter as tk
-from tkinter import *
-from tkinter.ttk import *
 root = tk.Tk()
-root.title("Borch News")
 root.geometry("500x750")
-combo = Combobox()
-combo["values"]= (parser.rubriks_name)
-combo.current(1) #Элемент выбранный по умолчанию
-combo.grid(column=0, row=0)
+root.title("Borch News")
+mainmenu = Menu(root) 
+root.config(menu=mainmenu) 
+menu = Menu(mainmenu, tearoff=0)
+menu2 = Menu(menu, tearoff=0)
+menu3 = Menu(menu, tearoff=0)
 
-root.mainloop()
+for i in parser.city_names:
+    menu2.add_radiobutton(label=i)
+ 
+menu.add_cascade(label="Города",menu=menu2)
+for i in parser.rubriks_name:
+    menu3.add_radiobutton(label=i)
+ 
+mainmenu.add_cascade(label="Категории",menu=menu)
+menu.add_cascade(label="Рубрики",menu=menu3)
+menu.add_separator()
+menu.add_command(label= "Выход", command = root.quit)
+ 
+root.mainloop() 
+
