dd3a132c3273823c193a13b3d863684aadb1012a
XRenso <70619857+XRenso@users.noreply.github.com>
2022-02-18 18:07:54 +1100

    Add files via upload

---
+++ mega_info-prikaz_generala_gavsa/gui.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+
+
+window = tk.Tk()
+window.geometry("400x650")
+
+
+
+
+button = tk.Button(
+    text="Мое имя",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+
+button.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
